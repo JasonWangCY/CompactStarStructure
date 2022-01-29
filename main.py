7d96523b8ca03f3d1c3f0cb644126e88c4f6c43b
JasonWangCY <wangcheukyeung@gmail.com>
2022-01-29 21:03:05 +0800

    Added graph to compare Newtonian and TOV

--- main.py
+++ main.py
@@ -23,32 +23,41 @@ def single_test(rho0=1e9):
 
 def MR_relation(rho_low=3, rho_high=13):
     r0 = 0.0001
-    # P0 = 2.38e16
-    rho0_list = np.logspace(rho_low, rho_high, 20)
+    rho0_list = np.logspace(rho_low, rho_high, 50)
     h = 10000
     totR_list = []
     totM_list = []
+    totR_list_TOV = []
+    totM_list_TOV = []
 
     eos = SolveEOS(r0, h)
 
     for rho0 in rho0_list:
         M0 = 4/3 * cg.pi * r0**3 * rho0
-        r_list, M_list, rho_list, P_list = eos.solve_TOS(M0, rho0, TOV=False, graph_flag=False)
-        # r_list, M_list, rho_list, P_list = fn.RK4(r0, M0, rho0, h, fn.EOS, fn.dMdr, fn.drhodr_Newt)
+
+        r_list, M_list, rho_list, P_list = eos.solve_TOS(M0, rho0, TOV=False, graph_flag=False)       
         total_R = r_list[-1].real
         total_M = M_list[-1].real
 
+        r_list_TOV, M_list_TOV, rho_list_TOV, P_list_TOV = eos.solve_TOS(M0, rho0, TOV=True, graph_flag=False) 
+        total_R_TOV = r_list_TOV[-1].real
+        total_M_TOV = M_list_TOV[-1].real
+
         totR_list.append(total_R)
         totM_list.append(total_M)
+        totR_list_TOV.append(total_R_TOV)
+        totM_list_TOV.append(total_M_TOV)
 
     plt.rcParams['figure.figsize'] = [10, 10]
     fig, ax = plt.subplots()
-    ax.plot(totM_list, totR_list)
+    ax.plot(totM_list, totR_list, c='b', label='Newtonian')
+    ax.plot(totM_list_TOV, totR_list_TOV, c='r', label='TOV')
     ax.set_title('Mass-radius relationship of WD')
     ax.set_ylabel(r'$r$ ($R_\odot$)')
     ax.set_xlabel(r'$M$ ($M_\odot$)')
     ax.tick_params(axis='both', which='major', direction = 'in')
+    ax.legend()
     plt.show()
 
-single_test(1e9)
-# MR_relation()
+# single_test(1e9)
+MR_relation()
